3a49cae7b2e96933d0e6a01975bcd6d5a60599fd
Icebabie <babeldivyansh28@gmail.com>
2026-01-18 00:23:48 +0530

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,5 +10,36 @@ from dataclasses import dataclass
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join("artifacts","train.csv")
-    testta_path: str=os.path.join("artifacts","train.csv")
+    test_data_path: str=os.path.join("artifacts","test.csv")
+    raw_data_path: str=os.path.join("artifacts","data.csv")
 
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def InitiateIngestion(self):
+        logging.info("entered data ingestion method/component")
+        try:
+            df=pd.read_csv("notebooks/diabetes.csv")
+
+            logging.info("Read the dataset as dataframe")
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+
+            logging.info("Train Test Split initiated")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False)
+            logging.info("Data Ingestion completed")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+if __name__ == "__main__":
+    obj=DataIngestion()
+    obj.InitiateIngestion()
